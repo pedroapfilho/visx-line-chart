8a32260294b233ccfa9117e99e9225526d393717
Pedro Filho <pedro@filho.me>
2021-05-16 17:17:11 -0300

    adding globalStyles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import Chart from "./Chart";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html,
+  body,
+  #root {
+    height: 100%;
+    width: 100%;
+  }
+
+`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <Chart />
+    <>
+      <GlobalStyle />
+      <Chart />
+    </>
   </React.StrictMode>,
   document.getElementById("root")
 );
